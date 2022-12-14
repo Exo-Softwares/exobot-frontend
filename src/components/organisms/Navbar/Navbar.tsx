072ad3ac2389b401c5202c3b41449527cd8c67fe
gfrnca <gabrielbfranca27@gmail.com>
2022-12-14 18:36:24 -0300

    Logo adicionada ao Navbar

--- src/components/organisms/Navbar/Navbar.tsx
+++ src/components/organisms/Navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 /* Navbar Component */
 
 /* Styles Imports */
@@ -5,11 +7,23 @@ import { Container } from "../../../styles/globals"
 import { ButtonDefault } from "../../atoms/Button.styled";
 import { NavbarStyles, StickyNavbar } from './Navbar.styled';
 
+import Image from 'next/image'
+
+/* Assets Imports */
+import DiscordityLogo from '../../../assets/discordityLogo.png';
+
 const Navbar = () => {
   return (
     <StickyNavbar>
       <Container>
         <NavbarStyles>
+          {/* Discordity Logo */}
+          <Image
+            src={DiscordityLogo}
+            alt="Discordity"
+            width={180}
+          />
+
           {/* Navbar Links */}
           <ul>
             <li>Como funciona</li>
