e25e54927fa55ad2d0d7ca76516101136cc75cac
gfrnca <gabrielbfranca27@gmail.com>
2023-05-23 11:21:32 -0300

    refactor: improving style code

--- src/components/organisms/Applications/ApplicationActiveButton/ApplicationActiveButton.styled.ts
+++ src/components/organisms/Applications/ApplicationActiveButton/ApplicationActiveButton.styled.ts
@@ -1,12 +1,12 @@
 /*  Application Active Button Component Styles */
 
 /* General Imports */
-import styled from 'styled-components';
+import styled from "styled-components";
 
 /* Theme Import */
-import theme from '../../../../styles/theme';
+import theme from "../../../../styles/theme";
 
-export const ApplicationActiveButtonStyles = styled.div `
+export const ApplicationActiveButtonStyles = styled.div`
   width: 100%;
   margin-bottom: 20px;
   display: flex;
@@ -22,7 +22,7 @@ export const ApplicationActiveButtonStyles = styled.div `
 
   p {
     font-size: 14px;
-    color: ${theme.colors.text}
+    color: ${theme.colors.text};
   }
 
   &:last-child {
@@ -37,30 +37,36 @@ export const ApplicationActiveButtonStyles = styled.div `
     display: flex;
     flex-direction: row;
     align-items: center;
-  }
 
-  .icon-container .icon {
-    width: 50px;
-    height: 50px;
-    background: black;
-    border-radius: 100%;
+    .icon-container {
+      .icon {
+        width: 50px;
+        height: 50px;
+        background: black;
+        border-radius: 100%;
+      }
+    }
+
+    .text-container {
+      margin-left: 20px;
+    }
   }
 
   .application-plan {
     margin-left: auto;
     font-size: 20px;
     font-weight: bold;
-  }
 
-  .application-plan p {
-    background: -webkit-linear-gradient(110deg, #4a4a4a, ${theme.colors.pricing.advanced});
-    font-size: 22px;
-    background-clip: text;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
+    p {
+      background: -webkit-linear-gradient(
+        110deg,
+        #4a4a4a,
+        ${theme.colors.pricing.advanced}
+      );
+      font-size: 22px;
+      background-clip: text;
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+    }
   }
-
-  .text-container {
-    margin-left: 20px;
-  }
-`
+`;
