d201fa48da4cc38eaf143b92ce59d64a48b2b4f0
gfrnca <gabrielbfranca27@gmail.com>
2023-06-01 15:12:31 -0300

    refactor: refactored simple navbar (dashboard)

--- src/components/organisms/Dashboard/SimpleNavbar/SimpleNavbar.styled.ts
+++ src/components/organisms/Dashboard/SimpleNavbar/SimpleNavbar.styled.ts
@@ -3,11 +3,16 @@ import styled from 'styled-components'
 export const SimpleNavbarWrapper = styled.header`
   width: 100%;
   height: 90px;
+  margin-bottom: 30px;
   display: flex;
   align-items: center;
   justify-content: space-between;
 
   .back-container {
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+
     .icon {
       font-size: 30px;
       cursor: pointer;
@@ -16,13 +21,35 @@ export const SimpleNavbarWrapper = styled.header`
         color: white;
       }
     }
+
+    .application-title {
+      @media screen and (max-width: 750px) {
+        display: none;
+      }
+    }
   }
 
-  .avatar-container {
-    width: 45px;
-    height: 45px;
-    position: relative;
+  .stuff {
     display: flex;
-    justify-content: center;
+    flex-direction: row;
+    align-items: center;
+    gap: 30px;
+
+    .notification-container {
+      width: 45px;
+      height: 45px;
+      position: relative;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .avatar-container {
+      width: 45px;
+      height: 45px;
+      position: relative;
+      display: flex;
+      justify-content: center;
+    }
   }
 `
--- src/components/organisms/Dashboard/SimpleNavbar/SimpleNavbar.tsx
+++ src/components/organisms/Dashboard/SimpleNavbar/SimpleNavbar.tsx
@@ -1,15 +1,26 @@
 import Avatar from '@/components/atoms/Avatar'
 import { SimpleNavbarWrapper } from './SimpleNavbar.styled'
 import { Icon } from '@/components/atoms/Icon'
+import Title from '@/components/atoms/Title'
+import Link from 'next/link'
+import Notifications from '@/components/atoms/Notifications'
 
 const SimpleNavbar = () => {
   return (
     <SimpleNavbarWrapper>
       <div className="back-container">
-        <Icon nameIcon="ImArrowLeft2" propsIcon={{ className: 'icon' }} />
+        <Link href="/applications">
+          <Icon nameIcon="ImArrowLeft2" propsIcon={{ className: 'icon' }} />
+        </Link>
+        <Title className="application-title">Nome da aplicação</Title>
       </div>
-      <div className="avatar-container">
-        <Avatar />
+      <div className="stuff">
+        <div className="notification-container">
+          <Notifications />
+        </div>
+        <div className="avatar-container">
+          <Avatar />
+        </div>
       </div>
     </SimpleNavbarWrapper>
   )
