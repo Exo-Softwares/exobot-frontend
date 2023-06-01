7e075f6bd90501c29d5b8a50f2bb3d707d7c1aca
gfrnca <gabrielbfranca27@gmail.com>
2023-05-31 22:16:45 -0300

    feat: created sidebar

--- src/components/organisms/Dashboard/Sidebar/Sidebar.styled.ts
+++ src/components/organisms/Dashboard/Sidebar/Sidebar.styled.ts
@@ -1,9 +1,35 @@
-/*  Sidebar Component Styles */
-
-/* General Imports */
 import styled from 'styled-components'
 
-export const SidebarStyles = styled.nav`
-  width: 500px;
+export const SidebarWrapper = styled.div`
+  width: 300px;
   height: 100%;
+  padding: 20px;
+  background: ${(props) => props.theme.colors.background};
+  border-right: 1px solid ${(props) => props.theme.colors.cardOutline};
+  box-shadow: rgba(0, 0, 0, 0.56) 0px 15px 20px 4px;
+
+  header {
+    margin-bottom: 40px;
+  }
+
+  nav {
+    ul {
+      li {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        list-style: none;
+        transition: 0.1s all;
+        cursor: pointer;
+
+        &:hover {
+          color: white;
+        }
+
+        .icon {
+          font-size: 25px;
+        }
+      }
+    }
+  }
 `
--- src/components/organisms/Dashboard/Sidebar/Sidebar.tsx
+++ src/components/organisms/Dashboard/Sidebar/Sidebar.tsx
@@ -1,11 +1,38 @@
-/* Sidebar Component */
+import Logo from '@/components/atoms/Logo'
+import { SidebarWrapper } from './Sidebar.styled'
+import { Icon } from '@/components/atoms/Icon'
+import { useEffect, useState } from 'react'
 
-import { SidebarStyles } from './Sidebar.styled'
-
-/* Styles Imports */
-
-const Menu = () => {
-  return <SidebarStyles></SidebarStyles>
+interface SidebarProps {
+  getContent: (content: number) => void
 }
 
-export default Menu
+const Sidebar = ({ getContent }: SidebarProps) => {
+  const [content, setContent] = useState(0)
+
+  useEffect(() => {
+    getContent(content)
+  }, [content])
+
+  return (
+    <SidebarWrapper>
+      <header>
+        <Logo />
+      </header>
+      <nav>
+        <ul>
+          <li onClick={() => setContent(1)}>
+            <Icon nameIcon="BsFillGearFill" propsIcon={{ className: 'icon' }} />
+            Configurações gerais
+          </li>
+          <li onClick={() => setContent(1)}>
+            <Icon nameIcon="BsFillGearFill" propsIcon={{ className: 'icon' }} />
+            Configurações gerais
+          </li>
+        </ul>
+      </nav>
+    </SidebarWrapper>
+  )
+}
+
+export default Sidebar
