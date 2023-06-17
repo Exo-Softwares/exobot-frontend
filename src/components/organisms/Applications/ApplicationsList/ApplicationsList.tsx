294c70cf768c6631e92b539b7cb636b8a7d3cb74
gfrnca <gabrielbfranca27@gmail.com>
2023-06-17 00:26:06 -0300

    feat: animation and scroll lock when creating app

--- src/components/organisms/Applications/ApplicationsList/ApplicationsList.tsx
+++ src/components/organisms/Applications/ApplicationsList/ApplicationsList.tsx
@@ -1,30 +1,64 @@
 import { RootState } from '@/store/store'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import ApplicationBuyButton from '../../../molecules/ApplicationBuyButton/ApplicationBuyButton'
 import { ApplicationsListWrapper } from './ApplicationsList.styled'
 import { useSelector } from 'react-redux'
 import Link from 'next/link'
-
 import ConfigModal from '@/components/molecules/ConfigModal/ConfigModal'
 import { Application as ApplicationProps } from '@/types/application'
 import Application from '@/components/molecules/Application/Application'
+import { animated, useTransition } from 'react-spring'
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
 
 const ApplicationsList = () => {
   const { applicationType, applications } = useSelector(
     (state: RootState) => state.applications,
   )
 
+  // Set which app is being created
   const [appBeingCreated, setAppBeingCreated] =
     useState<ApplicationProps | null>(null)
 
+  // App creation modal animation
+  const transition = useTransition(appBeingCreated, {
+    from: { x: 0, y: 800, opacity: 0 },
+    enter: { x: 0, y: 0, opacity: 1 },
+    leave: { x: 0, y: 800, opacity: 0 },
+  })
+
+  // Prevent user from scrolling when creating application
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const config = document.getElementById('config')
+
+      const targetElement: HTMLElement = config!
+
+      if (appBeingCreated) {
+        window.scrollTo({ top: 0 })
+        disableBodyScroll(targetElement)
+      } else {
+        enableBodyScroll(targetElement)
+      }
+    }
+  }, [appBeingCreated])
+
   return (
     <ApplicationsListWrapper>
-      {appBeingCreated && (
-        <ConfigModal
-          setAppBeingCreated={setAppBeingCreated}
-          appBeingCreated={appBeingCreated}
-        />
+      {transition(
+        (style, item) =>
+          item && (
+            <animated.div className="test" style={style}>
+              <ConfigModal
+                setAppBeingCreated={setAppBeingCreated}
+                appBeingCreated={item}
+              />
+            </animated.div>
+          ),
       )}
+
+      {/* {appBeingCreated && (
+
+      )} */}
 
       {applicationType === false ? (
         <div className="your-applications">
