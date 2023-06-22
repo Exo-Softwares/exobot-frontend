d95e5156fb1fb182755052e31c2b4bffe0143eab
gfrnca <gabrielbfranca27@gmail.com>
2023-06-22 18:31:50 -0300

    refactor: solving class names conflicts

--- src/components/organisms/Dashboard/Sections/Dash/Dash.styled.ts
+++ src/components/organisms/Dashboard/Sections/Dash/Dash.styled.ts
@@ -1,85 +1,83 @@
 import styled from 'styled-components'
 
 export const DashWrapper = styled.section`
-  .general-container {
-    .content-section {
+  .section-content {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row !important;
+    gap: 60px !important;
+
+    .app-profile {
       display: flex;
-      justify-content: space-between;
-      flex-direction: row !important;
-      gap: 60px !important;
+      flex-wrap: wrap;
+      flex-direction: row;
+      gap: 20px;
 
-      .app-profile {
+      @media (max-width: 728px) {
+        justify-content: center;
+      }
+
+      .app-avatar {
+        width: 130px;
+        height: 130px;
         display: flex;
-        flex-wrap: wrap;
-        flex-direction: row;
-        gap: 20px;
+        justify-content: center;
+        align-items: center;
+        background: #050505;
+        border-radius: 100%;
+        cursor: pointer;
 
-        @media (max-width: 728px) {
-          justify-content: center;
+        .icon {
+          color: #3d3d3d;
+          font-size: 70px;
         }
 
-        .app-avatar {
-          width: 130px;
-          height: 130px;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          background: #050505;
-          border-radius: 100%;
-          cursor: pointer;
-
+        &:hover {
           .icon {
-            color: #3d3d3d;
-            font-size: 70px;
-          }
-
-          &:hover {
-            .icon {
-              color: #adadad;
-            }
-          }
-        }
-
-        .app-data {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-
-          .title {
-            color: white;
-          }
-
-          .id {
-            display: flex;
-            align-items: center;
-            gap: 5px;
-            color: #404040;
-            font-size: 18px !important;
+            color: #adadad;
           }
         }
       }
 
-      .dash-info {
-        display: flex;
-        justify-content: space-between;
-        flex: 1;
-        gap: 30px;
-      }
-
-      .server-info {
+      .app-data {
         display: flex;
         flex-direction: column;
-        gap: 20px;
-        width: 100%;
+        align-items: center;
 
-        .info {
-          width: 100%;
-          display: flex;
-          flex-wrap: wrap;
-          gap: 30px;
-          flex-direction: row;
-          justify-content: space-between;
+        .title {
+          color: white;
         }
+
+        .id {
+          display: flex;
+          align-items: center;
+          gap: 5px;
+          color: #404040;
+          font-size: 18px !important;
+        }
+      }
+    }
+
+    .dash-info {
+      display: flex;
+      justify-content: space-between;
+      flex: 1;
+      gap: 30px;
+    }
+
+    .server-info {
+      display: flex;
+      flex-direction: column;
+      gap: 20px;
+      width: 100%;
+
+      .info {
+        width: 100%;
+        display: flex;
+        flex-wrap: wrap;
+        gap: 30px;
+        flex-direction: row;
+        justify-content: space-between;
       }
     }
   }
--- src/components/organisms/Dashboard/Sections/Dash/Dash.tsx
+++ src/components/organisms/Dashboard/Sections/Dash/Dash.tsx
@@ -8,52 +8,52 @@ import { AiFillTags } from 'react-icons/ai'
 const Dash = () => {
   return (
     <DashWrapper>
-      <Title fontSize="30px" weight="regular" className="content-title">
-        Dashboard<span>.</span>
-      </Title>
+      <div className="section-title">
+        <Title fontSize="30px" weight="regular" className="content-title">
+          Dashboard<span>.</span>
+        </Title>
+      </div>
 
-      <div className="general-container">
-        <div className="content-section">
-          <div className="app-profile">
-            <div className="app-avatar">
-              <IoMdAddCircle className="icon" />
-            </div>
-            <div className="app-data">
-              <Title className="title" fontSize="24px" center>
-                Nome da aplicação
-              </Title>
-              <Text className="id" center>
-                <AiFillTags className="icon" />
-                ID DA APLICAÇÃO
-              </Text>
-            </div>
+      <div className="section-content">
+        <div className="app-profile">
+          <div className="app-avatar">
+            <IoMdAddCircle className="icon" />
           </div>
-          <div className="dash-info">
-            <div className="field-group">
-              <Field label="Servidor" value="Nenhum" />
-            </div>
-            <div className="field-group">
-              <Field label="Plano" value="Standard" />
-              <Field label="Situação" value="Pago" />
-            </div>
+          <div className="app-data">
+            <Title className="title" fontSize="24px" center>
+              Nome da aplicação
+            </Title>
+            <Text className="id" center>
+              <AiFillTags className="icon" />
+              ID DA APLICAÇÃO
+            </Text>
           </div>
         </div>
+        <div className="dash-info">
+          <div className="field-group">
+            <Field label="Servidor" value="Nenhum" />
+          </div>
+          <div className="field-group">
+            <Field label="Plano" value="Standard" />
+            <Field label="Situação" value="Pago" />
+          </div>
+        </div>
+      </div>
 
-        <div className="content-section">
-          <div className="server-info">
-            <Title weight="regular">Nome do servidor</Title>
-            <div className="info">
-              <div className="field-group">
-                <Field label="Membros" value="1000" />
-                <Field label="Categorias" value="3" />
-              </div>
-              <div className="field-group">
-                <Field label="Cargos" value="22" />
-                <Field label="Canais de voz" value="10" />
-              </div>
-              <div className="field-group">
-                <Field label="Canais de texto" value="4" />
-              </div>
+      <div className="section-content">
+        <div className="server-info">
+          <Title weight="regular">Nome do servidor</Title>
+          <div className="info">
+            <div className="field-group">
+              <Field label="Membros" value="1000" />
+              <Field label="Categorias" value="3" />
+            </div>
+            <div className="field-group">
+              <Field label="Cargos" value="22" />
+              <Field label="Canais de voz" value="10" />
+            </div>
+            <div className="field-group">
+              <Field label="Canais de texto" value="4" />
             </div>
           </div>
         </div>
