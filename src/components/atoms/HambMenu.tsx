158c5689f341d6d24fc2a0d4d8773a730fcc3257
gfrnca <gabrielbfranca27@gmail.com>
2023-06-22 18:33:23 -0300

    refactor: changed hamb menu compnent styles

--- src/components/atoms/HambMenu.tsx
+++ src/components/atoms/HambMenu.tsx
@@ -3,10 +3,11 @@ import React from 'react'
 
 export const HambMenuWrapper = styled.div`
   width: 30px;
-  height: 20px;
+  gap: 8px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  position: relative;
 
   @media (min-width: 1100px) {
     display: none;
@@ -16,19 +17,38 @@ export const HambMenuWrapper = styled.div`
     width: 100%;
     height: 2px;
     background: white;
+    transition: 0.2s all;
   }
 `
 
 interface HambMenuProps {
   onClick: (e: React.MouseEvent<HTMLInputElement>) => void
+  mobileMenuStatus: boolean
 }
 
-const HambMenu = ({ onClick }: HambMenuProps) => {
+const HambMenu = ({ onClick, mobileMenuStatus }: HambMenuProps) => {
   return (
     <HambMenuWrapper onClick={onClick}>
-      <div className="line" />
-      <div className="line" />
-      <div className="line" />
+      <div
+        className="line"
+        style={
+          mobileMenuStatus
+            ? { transform: 'rotate(135deg)', position: 'absolute' }
+            : undefined
+        }
+      />
+      <div
+        className="line"
+        style={mobileMenuStatus ? { display: 'none' } : undefined}
+      />
+      <div
+        className="line"
+        style={
+          mobileMenuStatus
+            ? { transform: 'rotate(45deg)', position: 'absolute' }
+            : undefined
+        }
+      />
     </HambMenuWrapper>
   )
 }
